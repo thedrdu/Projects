3d7693d863982202508ee5c559c05a1fe7cb3750
Raymond J. Rosales <rrosal01@eecs.tufts.edu>
2022-05-16 15:59:55 -0400

    working on python

--- Wordle/app/Program.py
+++ Wordle/app/Program.py
@@ -1,14 +1,29 @@
 # a python transcribed version of the other one
 
-# file = open("WordList.txt", "r")
-# wordlist = file.read().splitlines()
-# print(wordlist)
-# file.close()
+# ord: str/char --> int
+# chr: int --> char
 
 wordlist = []
+wordle_values = dict()
+letter_occurs = dict()
 
-with open("WordList.txt", "r") as file:
-    wordlist = file.read().splitlines()
+green_letters = []
+yellow_letters = dict()
+gray_letters = []
 
-print(wordlist[0])
-print(wordlist[3])
+def main():
+    for i in range(97, 123):
+        letter_occurs[chr(i)] = 0
+
+    # try loop?
+    with open("WordList2.txt", "r") as file:
+        wordlist = file.read().splitlines()
+
+    for word in wordlist:
+        for c in word:
+            letter_occurs[c] = letter_occurs[c] + 1
+            
+    print(letter_occurs)
+
+if __name__ == "__main__":
+    main()
