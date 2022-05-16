a254734841279abe76d5d02d1b66be8ae720df28
Raymond J. Rosales <rrosal01@eecs.tufts.edu>
2022-05-16 16:42:56 -0400

    python main mostly done

--- Wordle/app/Program.py
+++ Wordle/app/Program.py
@@ -12,6 +12,7 @@ yellow_letters = dict()
 gray_letters = []
 
 def main():
+    solved = False
     for i in range(97, 123):
         letter_occurs[chr(i)] = 0
 
@@ -21,9 +22,33 @@ def main():
 
     for word in wordlist:
         for c in word:
-            letter_occurs[c] = letter_occurs[c] + 1
+            letter_occurs[c] += 1
             
-    print(letter_occurs)
+    visited_letters = []
+    occurence_sum = 0.0
+    for word in wordlist:
+        visited_letters.clear()
+        occurence_sum = 0.0
+        for c in word:
+            if not c in visited_letters:
+                occurence_sum += letter_occurs[c]
+            visited_letters.append(c)
+        wordle_values[word] = occurence_sum / (len(wordlist) * 5)
+
+    print("Ready!")
+
+    #TODO: check for errors and stuff
+    while not solved:
+        inp = input("Enter input or command: ")
+        if inp == "exit":
+            print("deez nuts lmao")
+        elif inp == "reset":
+            print("Whoa, I didn't implement that yet")
+            
+        data = inp.split(' ')
+        guess = generateGuess(data[0], data[1])
+        print("Try guessing", guess)
+
 
 if __name__ == "__main__":
     main()
