0561f6de52e2b4a9ff6f76f3d0218ab04ca0e4df
Raymond <rjrosales2025@gmail.com>
2022-05-17 17:42:36 -0400

    initial sudoku commit

---
+++ Sudoku/solver.cpp
@@ -0,0 +1,12 @@
+/* sudoku solver
+ * This program solves a sudoku puzzle
+ * Written by thedrdu and rayros2025, 2022-05-17
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main() {
+    return 0;
+ }
