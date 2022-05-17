0da04156c88b1a5693c97e20257c5fe632e5f90e
Raymond <rjrosales2025@gmail.com>
2022-05-17 17:50:32 -0400

    adding basic code

--- Sudoku/solver.cpp
+++ Sudoku/solver.cpp
@@ -4,9 +4,26 @@
  */
 
  #include <iostream>
+ #include <fstream>
 
  using namespace std;
 
  int main() {
+    cout << "Enter filename: ";
+    string filename;
+    cin >> filename;
+    ifstream infile;
+    infile.open(filename);
+    if (not infile.is_open()) {
+        cerr << "ERROR: not valid file\n";
+        exit (EXIT_FAILURE);
+    }
+    int grid[9][9];
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            infile >> grid[i][j];
+        }
+    }
+    infile.close();
     return 0;
  }
